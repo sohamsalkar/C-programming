737b88726f3b80ae0b4d853fe926f637ab0d718f
Soham Salkar <63644015+sohamsalkar@users.noreply.github.com>
2021-02-22 20:46:40 +0530

    Add files via upload

--- Lec10/8sizeof.c
+++ Lec10/8sizeof.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
-int main() // warnings because of sizeof returns %lu unsigned long
+int main()
 {
     void *ptr = NULL; //void pointer
-    int *p = NULL;    // integer pointer
+    int *p = NULL;    // integer pointer to point integer values
     int a = 10;
-    char *cp = NULL; //character pointer
+    char *cp = NULL; //character pointer to point charachter
     char ch = 'h';
-    float *fp = NULL; //float pointer
+    float *fp = NULL; //float pointer to point float values
     float f = 7.42;
+
+    //sizeof() // warnings because of sizeof returns %lu unsigned long size
+
     //size of void pointer
     printf("size of void pointer = %d\n", sizeof(ptr));
     //size of integer pointer
     printf("size of integer pointer = %d\n", sizeof(p));
-    printf("size of int = %d\n", sizeof(a));
+    printf("size of int = %lu\n", sizeof(a));
     //size of character pointer
     printf("size of character pointer = %d\n", sizeof(cp));
-    printf("size of char = %d\n", sizeof(ch));
+    printf("size of char = %d\n", sizeof(ch)); // ASCII
     //size of float pointer
     printf("size of float pointer = %d\n", sizeof(fp));
-    printf("size of float = %d\n", sizeof(f));
+    printf("size of float = %d\n", sizeof(f)); //7.353523
     return 0;
 }
